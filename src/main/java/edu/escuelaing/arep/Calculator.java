ae28c118e68f24be2acefc3f2ef5e99b0e14bfff
unknown <juan.espinosa-t@labinfo.is.escuelaing.edu.co>
2022-03-30 14:00:24 -0500

    first to local

---
+++ src/main/java/edu/escuelaing/arep/Calculator.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep;
+import java.lang.Math;
+
+public class Calculator {
+
+    public Double sqrt(Double number){
+        return Math.sqrt(number);
+    }
+
+    public Double sin(Double number){
+        return Math.sin(number);
+    }
+}
